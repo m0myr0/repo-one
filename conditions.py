3e46b8c1e5ad29e95ea3e7177e5d4c55e0c49929
m0myr0 <momy_ro@yahoo.fr>
2016-05-05 23:31:04 +0200

    made some changes

--- conditions.py
+++ conditions.py
@@ -1,9 +1,9 @@
 import time
 print("numbers between 33 and 77")
 for x in range(100):
-     if 33 <= 2*x <= 77:  # plus concis et plus lisible
-         print(x, "not in range")
+     if x>= 33 and x <=77 : 
+              print(x, "in range")
      else:
-         print(x, "in range")
+         print(x, "not in range")
 #this is just for windows cmd(so it doesn't close to quick)
 time.sleep(7)
