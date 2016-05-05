804be9d571de46fefa5e111ea4688f328f7adb1e
m0myr0 <momy_ro@yahoo.fr>
2016-05-05 23:00:08 +0200

    hello world

---
+++ helloWorld.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import time
+print("hello World")
+print()
+print("Its my first python programming !!!")
+print()
+print("cette fois je peut mettre des caractères spécieux comme en français c'est cool")
+#this is an quote
+print()
+print("now it's your turn to speak")
+mess=input("comom don't be shy : ")
+print(mess)
+print()
+print("it si really quite easy to make a firs print program with myton like almost every other programming languages")
+# Wait for 7 seconds
+time.sleep(7)
+=======
+print(mess)
