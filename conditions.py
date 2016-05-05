fcc19e5091ca005ff1b87255919ada169c0e4a08
m0myr0 <momy_ro@yahoo.fr>
2016-05-05 23:17:05 +0200

    add new file for conditions

---
+++ conditions.py
@@ -0,0 +1,8 @@
+import time
+print("numbers between 33 and 77")
+for x in range(100):
+     if 33 <= 2*x <= 77:  # plus concis et plus lisible
+         print(x, "not in range")
+     else:
+         print(x, "in range")
+time.sleep(7)
